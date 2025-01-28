0840c13a65e38a69bb3440a44576f72e5a37fb10
Volodymyr SF <safjanik.volody@gmail.com>
2025-01-28 12:37:39 +0200

    fix some bugs

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,8 +5,8 @@ import { Config } from './config.type';
 
 export default (): Config => ({
   app: {
-    port: parseInt(process.env.APP_PORT ?? '3000', 10),
-    host: process.env.APP_HOST ?? 'localhost',
+    port: parseInt(process.env.APP_PORT, 10),
+    host: process.env.APP_HOST,
   },
   database: {
     host: process.env.DB_HOST,
